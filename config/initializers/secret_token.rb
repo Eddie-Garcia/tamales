4e2d097ef6ecf217d8eeb6ab2ccd3963bf61409e
Eddie-Garcia <g_eduardo90@yahoo.com>
2016-05-06 17:20:17 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tamales::Application.config.secret_key_base = 'ea62309990c7687dee12f9d4fb27c50aa9a584efbc8115b5e1389ec0c7966214a4a519a394983d940afd71afa403d4139efc4f8c1d52353483a130fd9124134f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tamales::Application.config.session_store :cookie_store, key: '_tamales_session'
